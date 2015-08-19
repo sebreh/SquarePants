9a38c964f64ef280e3edf7371d01693b8fe2b716
Sebastian Rehnby <sebastian@rehnby.com>
2015-08-19 07:42:27 +0200

    Initial Commit

---
+++ SquarePants/SquarePants.h
@@ -0,0 +1,19 @@
+//
+//  SquarePants.h
+//  SquarePants
+//
+//  Created by Sebastian Rehnby on 19/08/15.
+//  Copyright © 2015 Peakon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SquarePants.
+FOUNDATION_EXPORT double SquarePantsVersionNumber;
+
+//! Project version string for SquarePants.
+FOUNDATION_EXPORT const unsigned char SquarePantsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SquarePants/PublicHeader.h>
+
+
